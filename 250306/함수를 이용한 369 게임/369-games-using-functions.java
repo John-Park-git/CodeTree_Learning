fa282ddd6f6ea5079598799d8b46eae7c1290116
Park Yo Han <john38317@kw.ac.kr>
2025-03-06 19:33:29 +0900

    Title: 함수를 이용한 369 게임, Time: 150ms, Memory: 10MB, Status: Passed - CodeTree

--- 250306/함수를 이용한 369 게임/369-games-using-functions.java
+++ 250306/함수를 이용한 369 게임/369-games-using-functions.java
@@ -12,13 +12,11 @@ public class Main {
         return n%3==0;
     }
     public static boolean checkCondition2(int n) {
-        if(n<10 && (n == 3 || n == 6 || n == 9))
+        while(n>0) {
+            if(n%10 ==3||n%10==6||n%10==9) {
                 return true;
-        if(n<100 && (n%10 == 3 || n%10 == 6 || n%10 == 9))
-                return true;
-        for(int i=10;i<=100000;i*=10) {
-            if(n/i == 3 || n/i == 6 || n/i == 9 )
-                return true;
+            }
+            n /= 10;
         }
         return false;
     }
