1d69bb30579f879e588e715fcb502b6e728ce36b
Park Yo Han <john38317@kw.ac.kr>
2025-03-06 19:53:18 +0900

    Title: 함수를 이용한 소수 판별, Time: 137ms, Memory: 10MB, Status: Passed - CodeTree

--- 250306/함수를 이용한 소수 판별/decimal-decisions-using-functions.java
+++ 250306/함수를 이용한 소수 판별/decimal-decisions-using-functions.java
@@ -10,6 +10,8 @@ public class Main {
         return sum;
     }
     public static boolean isPrime(int n) {
+        if(n==1)
+            return false;
         for(int i=2;i<=n-1;i++) {
             if(n%i==0)
                 return false;
