a64022ea070b48f0bc3559356a378c16d94fad98
Park Yo Han <john38317@kw.ac.kr>
2025-03-09 17:33:48 +0900

    Title: 사칙연산 함수, Time: 132ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 250309/사칙연산 함수/quadratic-operations-function.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+public class Main {
+    public static void plus(int a, char o, int c) {
+            System.out.println(a + " + " + c + " = " + (a+c));
+    }
+    public static void minus(int a, char o, int c) {
+            System.out.println(a + " - " + c + " = " + (a-c));
+    }
+    public static void product(int a, char o, int c) {
+            System.out.println(a + " * " + c + " = " + (a*c));
+    }
+    public static void divide(int a, char o, int c) {
+            System.out.println(a + " / " + c + " = " + (a/c)); 
+    }
+    public static void calc(int a, char o, int c) {
+        if(o!='+' && o!='-' && o!='*' && o!='/') {
+            System.out.println("False");
+        }
+        if(o=='+') 
+            plus(a,o,c);
+        
+        else if(o=='-') 
+            minus(a,o,c);
+        
+        else if(o=='*') 
+            product(a,o,c);
+    
+        else if(o=='+') 
+            divide(a,o,c);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        char o = sc.next().charAt(0);
+        int c = sc.nextInt();
+        // Please write your code here.
+        calc(a,o,c);
+    }
+}
