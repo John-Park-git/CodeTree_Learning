72d3d567c158d68100d6065da60ab9003d82254c
Park Yo Han <john38317@kw.ac.kr>
2025-03-17 15:31:52 +0900

    Title: 두 정수에 대한 연산값, Time: 135ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 250317/두 정수에 대한 연산값/operational-values-for-two-integers.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void modify(Num n1, Num n2) {
+        if(n1.num > n2.num) {
+            n1.num +=25;
+            n2.num *=2;
+        }
+        else if(n2.num>n1.num) {
+            n1.num *=2;
+            n2.num += 25;
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        // Please write your code here.
+        Num num1 = new Num(a);
+        Num num2 = new Num(b);
+        modify(num1,num2);
+        System.out.println(num1.num + " " + num2.num);
+    }
+}
+class Num {
+        public int num;
+        public Num(int n) {
+            this.num = n;
+        }
+    }
