77d1d7abc5efdc7325db6dfe408df846acc2ab90
Park Yo Han <john38317@kw.ac.kr>
2025-03-17 20:31:35 +0900

    Title: 재귀함수를 이용한 별 출력 2, Time: 155ms, Memory: 11MB, Status: Passed - CodeTree

---
+++ 250317/재귀함수를 이용한 별 출력 2/star-output-with-recursive-function-2.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Main {
+    public static void star(int n) {
+        if(n==0)
+            return;
+        for(int i=1;i<=n;i++) {
+            System.out.print("*"+" ");
+        }
+        System.out.println();
+        star(n-1);
+        for(int i=1;i<=n;i++) {
+            System.out.print("*"+" ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        // Please write your code here.
+        star(n);
+    }
+}
