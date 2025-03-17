9b83815791bd8f9ecc35eb16a7f4caa256f61a15
Park Yo Han <john38317@kw.ac.kr>
2025-03-17 20:19:11 +0900

    Title: 숫자 차례로 출력하기, Time: 118ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 250317/숫자 차례로 출력하기/to-output-numerically.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Main {
+    public static void recursion1(int n) {
+        if(n==0)
+            return;
+        recursion1(n-1);
+        System.out.print(n+" ");
+    }
+    public static void recursion2(int n) {
+        if(n==0)
+            return;
+        System.out.print(n+" ");
+        recursion1(n-1);
+        
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        // Please write your code here.
+        recursion1(n);
+        System.out.println();
+        recursion2(n);
+
+    }
+}
