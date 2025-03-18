dccae33c14bca1b5945d61618040d6ed2de222d9
Park Yo Han <john38317@kw.ac.kr>
2025-03-18 12:26:08 +0900

    Title: 재귀함수를 이용한 최댓값, Time: 128ms, Memory: 10MB, Status: Runtime Error - CodeTree

---
+++ 250318/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Main {
+    public static int Max(int[]arr, int n) {
+        if(n==2)    
+            return arr[1]>arr[0]?arr[1]:arr[0];
+        if(arr[n-1]>arr[n-2]) {
+            int temp=arr[n-2];
+            arr[n-2]=arr[n-1];
+            arr[n-1]=arr[n-2];
+            return Max(arr,n-1);
+        }
+        else {
+            return Max(arr,n-1);
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        // Please write your code here.
+        System.out.println(Max(arr,n));
+    }
+}
