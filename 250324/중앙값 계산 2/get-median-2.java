01cb222798b16b6a2978f968a3b292ac916513cc
Master Jiren <john38317@kw.ac.kr>
2025-03-24 19:20:09 +0900

    Title: 중앙값 계산 2, Time: 132ms, Memory: 10MB, Status: Passed - CodeTree

--- 250324/중앙값 계산 2/get-median-2.java
+++ 250324/중앙값 계산 2/get-median-2.java
@@ -8,19 +8,17 @@ public class Main {
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
-        int cap = (n%2==0)? n-1:n;
+        int cap = (n%2!=0)? n:n-1;
         int [] mid = new int[cap];
-        int used =0;
+        int used = 0;
 
         for(int i=0;i<n;i++) {
-            mid[used]=arr[i];
-            ++used;
+            mid[used++]=arr[i];
             if((i+1)%2!=0) {
                 Arrays.sort(mid, 0, used);
                 System.out.print(mid[used/2] + " ");
             }
-        }   
-        
+        }     
         // Please write your code here.
     }
 }
